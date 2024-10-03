59dab86709b1e72a2729c14148b6e59a9a03acf1
lastdove <tpqms9917@gmail.com>
2024-10-04 06:59:46 +0900

    schedule app create

--- src/main/java/com/sparta/sparta/scheduleapp/dto/ScheduleRequestDTO.java
+++ src/main/java/com/sparta/sparta/scheduleapp/dto/ScheduleRequestDTO.java
@@ -1,25 +1,12 @@
 package com.sparta.sparta.scheduleapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class ScheduleRequestDTO {
     private final String todo;
     private final String author;
     private final String password;
-
-    public ScheduleRequestDTO(String todo, String author, String password) {
-        this.todo = todo;
-        this.author = author;
-        this.password = password;
-    }
-
-    public String getTodo() {
-        return todo;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 }
--- src/main/java/com/sparta/sparta/scheduleapp/dto/ScheduleResponseDTO.java
+++ src/main/java/com/sparta/sparta/scheduleapp/dto/ScheduleResponseDTO.java
@@ -1,60 +1,15 @@
 package com.sparta.sparta.scheduleapp.dto;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class ScheduleResponseDTO {
     private Long id; // ID of the schedule
     private String todo; // Task description
     private String author; // Author's name
     private LocalDateTime createdAt; // Creation date and time
     private LocalDateTime updatedAt; // Last update date and time
-
-    public ScheduleResponseDTO(Long id, String todo, String author, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        this.id = id;
-        this.todo = todo;
-        this.author = author;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTodo() {
-        return todo;
-    }
-
-    public void setTodo(String todo) {
-        this.todo = todo;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
-
