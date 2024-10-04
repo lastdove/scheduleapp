ec90af99d46e78fade60fd4b47a9317f3f2bac87
lastdove <tpqms9917@gmail.com>
2024-10-04 10:11:45 +0900

    Update ScheduleResponseDTO.java

--- src/main/java/com/sparta/sparta/scheduleapp/dto/ScheduleResponseDTO.java
+++ src/main/java/com/sparta/sparta/scheduleapp/dto/ScheduleResponseDTO.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class ScheduleResponseDTO {
-    private Long id; // ID of the schedule
-    private String todo; // Task description
-    private String author; // Author's name
-    private LocalDateTime createdAt; // Creation date and time
-    private LocalDateTime updatedAt; // Last update date and time
+    private Long id;
+    private String todo;
+    private String author;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt; 
 }
